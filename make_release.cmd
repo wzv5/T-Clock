592f6ec998e12b319693aab73c62a24fc1ef9b12
White-Tiger <White-Tiger@users.noreply.github.com>
2013-09-02 22:09:49 +0200

    * changed make_release.cmd again... it now creates archives within Release folder and removes manifest files (needed for non VC builds)

--- make_release.cmd
+++ make_release.cmd
@@ -1,9 +1,13 @@
 @echo off
 set PATH=%PATH%;C:\Program Files\7-Zip
-del Release\Win32\*.exp 2>nul
-del Release\Win32\*.lib 2>nul
-del Release\x64\*.exp 2>nul
-del Release\x64\*.lib 2>nul
-7z a T-Clock.zip .\Release\*
-7z a T-Clock.7z .\Release\*
+pushd Release
+del *.zip 2>nul
+del *.7z 2>nul
+del /S *.exp 2>nul
+del /S *.exp 2>nul
+del /S *.lib 2>nul
+del /S *.manifest 2>nul
+7z a T-Clock.zip .
+7z a -x!*.zip T-Clock.7z .
+popd
 pause
