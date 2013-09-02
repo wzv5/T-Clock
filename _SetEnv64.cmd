341a60c88eb92591879d3e173d86edbdfa2b94a7
White-Tiger <White-Tiger@users.noreply.github.com>
2013-09-02 21:18:56 +0200

    * updated project files and unified VC++ project files. + added VC++ project file for Calendar + added build script/environment scripts for WinSDK command line build

---
+++ _SetEnv64.cmd
@@ -0,0 +1,14 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+set sdkenv="C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+rem call %sdkenv% /debug
+call %sdkenv% /release
+set xmsbuild=msbuild /p:TargetFrameworkMoniker=".NETFramework,Version=v3.5" /p:Configuration="Release" /p:Platform="x64"
+rem set xmsbuild=msbuild /p:Configuration="Release" /p:Platform="x64"
+echo __________________________________________
+echo %%xmsbuild%% /t:Clean "T-Clock 2010.sln"
+echo.
+echo %%xmsbuild%% "T-Clock 2010.sln"
+echo.
+echo __________________________________________
+cmd /K
