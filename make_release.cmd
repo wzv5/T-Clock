cb7db6f8aaf6cf41fda3ee2beb038a4093702f53
White-Tiger <White-Tiger@users.noreply.github.com>
2013-09-02 21:27:34 +0200

    * changed make_release.cmd to be more user independent

--- make_release.cmd
+++ make_release.cmd
@@ -1,7 +1,9 @@
 @echo off
+set PATH=%PATH%;C:\Program Files\7-Zip
 del Release\Win32\*.exp 2>nul
 del Release\Win32\*.lib 2>nul
 del Release\x64\*.exp 2>nul
 del Release\x64\*.lib 2>nul
-7za a T-Clock.zip .\Release\*
+7z a T-Clock.zip .\Release\*
+7z a T-Clock.7z .\Release\*
 pause
