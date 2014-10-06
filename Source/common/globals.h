wzv5/T-Clock

commit 092acbb8f732ebddc77a8eea10b1d589e0cdfad5

* moved GetStartupFile,RemoveStartup,AddStartup,CreateLink from pageabout to main

Source/common/globals.h
@@ -28,9 +28,16 @@
 #define ARCH_SUFFIX_64 "64"
 #ifndef __x86_64__
 #	define ARCH_SUFFIX ARCH_SUFFIX_32
+#	define TCLOCK_SUFFIX " x64"
 #else
 #	define ARCH_SUFFIX ARCH_SUFFIX_64
+#	define TCLOCK_SUFFIX ""
 #endif // __x86_64__
+
+#define ABT_TITLE "T-Clock Redux" TCLOCK_SUFFIX " - " VER_SHORT_DOTS " build " STR(VER_REVISION)
+#define ABT_TCLOCK "T-Clock 2010 is Stoic Joker's rewrite of their code which allows it to run on Windows XP and up. While he removed some of T-Clock's previous functionality. He felt this makes it a more \"Administrator Friendly\" application as it no longer required elevated privileges to run.\n\nT-Clock Redux tries to continue Stoic Joker's efforts."
+#define CONF_START "T-Clock Redux" TCLOCK_SUFFIX
+#define CONF_START_OLD "Stoic Joker's T-Clock 2010" TCLOCK_SUFFIX
 
 #include "resource.h"
 

Source/Clock/PageMouse.c
@@ -3,6 +3,7 @@
 //-------------------------------------------------------*/
 // Modified by Stoic Joker: Saturday, March 6, 2010 - 8:11:17pm
 #include "tclock.h"
+#include <ShlObj.h>//SHBrowseForFolder
 
 static void OnInit(HWND hDlg,HWND* hList);
 static void OnApply(HWND hDlg);

Source/Clock/main.c
@@ -5,6 +5,7 @@
 #include "tclock.h" //---------------{ Stoic Joker 2006-2011 }---------------+++-->
 #include <winver.h>
 #include <wtsapi32.h>
+#include <ShlObj.h>//SHGetFolderPath
 #include "../common/version.h"
 #include "../common/tcolor.h" // WM_DWMCOLORIZATIONCOLORCHANGED
 
@@ -95,6 +96,87 @@ void RegisterSession(HWND hwnd)   //---------{ Explicitly Linked for Windows 200
 		}
 		FreeLibrary(handle);
 	}
+}
+//====================================================================================
+//---------------------------+++--> Does our startup file exist? Also creates filename:
+int GetStartupFile(HWND hDlg,char filename[MAX_PATH]){   //--------------------+++-->
+	size_t offset;
+	if(SHGetFolderPath(hDlg,CSIDL_STARTUP,NULL,SHGFP_TYPE_CURRENT,filename)!=S_OK){
+		return 0;
+	}
+	offset=strlen(filename);
+	filename[offset]='\\';
+	filename[offset+1]='\0'; // old Stoic Joker link
+	strcat(filename,CONF_START_OLD);
+	strcat(filename,".lnk");
+	if(PathFileExists(filename))
+		return 1;
+	filename[offset+1]='\0'; // new name
+	strcat(filename,CONF_START);
+	strcat(filename,".lnk");
+	if(PathFileExists(filename))
+		return 1;
+	return 0;
+}
+//================================================================================================
+//----------------------------------------+++--> Remove Launch T-Clock on Windows Startup ShortCut:
+void RemoveStartup(HWND hDlg)   //----------------------------------------------------------+++-->
+{
+	char path[MAX_PATH];
+	if(!GetStartupFile(hDlg,path))
+		return;
+	DeleteFile(path);
+}
+//===================================
+void AddStartup(HWND hDlg) //--+++-->
+{
+	char path[MAX_PATH], myexe[MAX_PATH];
+	if(GetStartupFile(hDlg,path) || !*path)
+		return;
+	*strrchr(path,'\\')='\0';
+	GetModuleFileName(GetModuleHandle(NULL),myexe,MAX_PATH);
+	CreateLink(myexe,path,CONF_START);
+}
+//==========================
+//--+++--> Create Launch T-Clock on Windows Startup ShortCut:
+int CreateLink(LPCSTR fname, LPCSTR dstpath, LPCSTR name)
+{
+	HRESULT hres;
+	IShellLink* psl;
+	
+	CoInitializeEx(NULL,COINIT_APARTMENTTHREADED);
+	
+	hres = CoCreateInstance(&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, &IID_IShellLink, (LPVOID*)&psl);
+	if(SUCCEEDED(hres)) {
+		IPersistFile* ppf;
+		char path[MAX_PATH];
+		
+		psl->lpVtbl->SetPath(psl, fname);
+		psl->lpVtbl->SetDescription(psl, name);
+		strcpy(path, fname);
+		del_title(path);
+		psl->lpVtbl->SetWorkingDirectory(psl, path);
+		
+		hres = psl->lpVtbl->QueryInterface(psl, &IID_IPersistFile, (LPVOID*)&ppf);
+		if(SUCCEEDED(hres)) {
+			WORD wsz[MAX_PATH];
+			char lnkfile[MAX_PATH];
+			strcpy(lnkfile, dstpath);
+			add_title(lnkfile, (char*)name);
+			strcat(lnkfile, ".lnk");
+			
+			MultiByteToWideChar(CP_ACP, 0, lnkfile, -1, wsz, MAX_PATH);
+			
+			hres = ppf->lpVtbl->Save(ppf, wsz, TRUE);
+			ppf->lpVtbl->Release(ppf);
+		}
+		psl->lpVtbl->Release(psl);
+	}
+	CoUninitialize();
+	
+	if(SUCCEEDED(hres))
+		return 1;
+	return 0;
 }
 
 //================================================================================================

Source/Clock/menu.c
@@ -2,6 +2,7 @@
 //--+++--> menu.c - pop-up menu on right button click - KAZUBON 1997-2001 =========
 //================= Last Modified by Stoic Joker: Wednesday, 12/22/2010 @ 11:29:24pm
 #include "tclock.h" //---------------{ Stoic Joker 2006-2010 }---------------+++-->
+#include <ShlObj.h>//IShellDispatch4
 
 void UpdateAlarmMenu(HMENU hMenu);
 static char g_undo=0; // did we change windows and can undo it?

Source/Clock/pageabout.c
@@ -5,16 +5,6 @@
 #include "tclock.h"
 #include "../common/version.h"
 
-#ifdef _WIN64
-#	define TCLOCK_SUFFIX " x64"
-#else
-#	define TCLOCK_SUFFIX ""
-#endif
-#define ABT_TITLE "T-Clock Redux" TCLOCK_SUFFIX " - " VER_SHORT_DOTS " build " STR(VER_REVISION)
-#define ABT_TCLOCK "T-Clock 2010 is Stoic Joker's rewrite of their code which allows it to run on Windows XP and up. While he removed some of T-Clock's previous functionality. He felt this makes it a more \"Administrator Friendly\" application as it no longer required elevated privileges to run.\n\nT-Clock Redux tries to continue Stoic Joker's efforts."
-#define CONF_START "T-Clock Redux" TCLOCK_SUFFIX
-#define CONF_START_OLD "Stoic Joker's T-Clock 2010" TCLOCK_SUFFIX
-
 static WNDPROC m_oldLabProc = NULL;
 static HCURSOR m_hCurHand = NULL;
 
@@ -24,10 +14,6 @@ static void OnLinkClicked(HWND hDlg, UINT id);
 
 LRESULT CALLBACK LabLinkProc(HWND, UINT, WPARAM, LPARAM);
 
-BOOL GetStartupFile(HWND hDlg,char filename[MAX_PATH]);
-void AddStartup(HWND hDlg);
-void RemoveStartup(HWND hDlg);
-BOOL CreateLink(LPCSTR fname, LPCSTR dstpath, LPCSTR name);
 #define SendPSChanged(hDlg) SendMessage(GetParent(hDlg),PSM_CHANGED,(WPARAM)(hDlg),0)
 /////////////////////////////////////////////////////////////////////////////////////
 INT_PTR CALLBACK PageAboutProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
@@ -137,86 +123,4 @@ LRESULT CALLBACK LabLinkProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPar
 		return TRUE;
 	}
 	return CallWindowProc(m_oldLabProc, hwnd, message, wParam, lParam);
-}
-//================================================================================
-//---------------------------+++--> Does startup file exist? Also creates filename:
-BOOL GetStartupFile(HWND hDlg,char filename[MAX_PATH]){   //-------------------------+++-->
-	size_t offset;
-	if(SHGetFolderPath(hDlg,CSIDL_STARTUP,NULL,SHGFP_TYPE_CURRENT,filename)!=S_OK){
-		return 0;
-	}
-	offset=strlen(filename);
-	filename[offset]='\\';
-	filename[offset+1]='\0'; // old Stoic Joker link
-	strcat(filename,CONF_START_OLD);
-	strcat(filename,".lnk");
-	if(PathFileExists(filename))
-		return 1;
-	filename[offset+1]='\0'; // new name
-	strcat(filename,CONF_START);
-	strcat(filename,".lnk");
-	if(PathFileExists(filename))
-		return 1;
-	return 0;
-}
-//================================================================================================
-//----------------------------------------+++--> Remove Launch T-Clock on Windows Startup ShortCut:
-void RemoveStartup(HWND hDlg)   //----------------------------------------------------------+++-->
-{
-	char path[MAX_PATH];
-	if(!GetStartupFile(hDlg,path))
-		return;
-	DeleteFile(path);
-}
-//======================================
-//--+++-->
-void AddStartup(HWND hDlg)
-{
-	char path[MAX_PATH], myexe[MAX_PATH];
-	if(GetStartupFile(hDlg,path) || !*path)
-		return;
-	*strrchr(path,'\\')='\0';
-	GetModuleFileName(GetModuleHandle(NULL),myexe,MAX_PATH);
-	CreateLink(myexe,path,CONF_START);
-}
-//==========================
-//--+++--> Create Launch T-Clock on Windows Startup ShortCut:
-BOOL CreateLink(LPCSTR fname, LPCSTR dstpath, LPCSTR name)
-{
-	HRESULT hres;
-	IShellLink* psl;
-	
-	CoInitializeEx(NULL,COINIT_APARTMENTTHREADED);
-	
-	hres = CoCreateInstance(&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, &IID_IShellLink, (LPVOID*)&psl);
-	if(SUCCEEDED(hres)) {
-		IPersistFile* ppf;
-		char path[MAX_PATH];
-		
-		psl->lpVtbl->SetPath(psl, fname);
-		psl->lpVtbl->SetDescription(psl, name);
-		strcpy(path, fname);
-		del_title(path);
-		psl->lpVtbl->SetWorkingDirectory(psl, path);
-		
-		hres = psl->lpVtbl->QueryInterface(psl, &IID_IPersistFile, (LPVOID*)&ppf);
-		
-		if(SUCCEEDED(hres)) {
-			WORD wsz[MAX_PATH];
-			char lnkfile[MAX_PATH];
-			strcpy(lnkfile, dstpath);
-			add_title(lnkfile, (char*)name);
-			strcat(lnkfile, ".lnk");
-			
-			MultiByteToWideChar(CP_ACP, 0, lnkfile, -1, wsz, MAX_PATH);
-			
-			hres = ppf->lpVtbl->Save(ppf, wsz, TRUE);
-			ppf->lpVtbl->Release(ppf);
-		}
-		psl->lpVtbl->Release(psl);
-	}
-	CoUninitialize();
-	
-	if(SUCCEEDED(hres)) return TRUE;
-	else return FALSE;
 }

Source/Clock/settings.c
@@ -123,9 +123,7 @@ int CheckSettings(){
 	}
 	return 0;
 }
-// temporarily forward defines from pageabout.c
-extern BOOL GetStartupFile(HWND hDlg,char filename[MAX_PATH]);
-extern void AddStartup(HWND hDlg);
+
 void ConvertSettings(){
 	char buf[MAX_PATH];
 	int idx, idx2;

Source/Clock/tclock.h
@@ -9,7 +9,6 @@
 #include <stdio.h>//sprintf
 #include <Uxtheme.h>//SetWindowTheme
 #include <WindowsX.h>//Edit_SetText
-#include <ShlObj.h>//IShellDispatch4
 #include <Shlwapi.h>//PathFileExists
 #include <Psapi.h>//EmptyWorkingSet
 #include "../common/newapi.h"
@@ -60,6 +59,10 @@ extern BOOL bMonOffOnLock; //-+> Locking Workstation Turns Off Monitor(s).
 void RegisterSession(HWND hwnd);
 void UnregisterSession(HWND hwnd);
 void ToggleCalendar(int type);
+int GetStartupFile(HWND hDlg,char filename[MAX_PATH]);
+void AddStartup(HWND hDlg);
+void RemoveStartup(HWND hDlg);
+int CreateLink(LPCSTR fname, LPCSTR dstpath, LPCSTR name);
 
 // settings.c
 int CheckSettings();

commit bd888fdf0af8f038f4c107142280147cdd07d7f9

! fixed bug introduced in commit 092acbb relating to startup file name... I've inversed the 32bit vs 64bit logic... so the 32bit startup file was named like the 64bit one and vice versa :P

Source/common/globals.h
@@ -28,10 +28,10 @@
 #define ARCH_SUFFIX_64 "64"
 #ifndef __x86_64__
 #	define ARCH_SUFFIX ARCH_SUFFIX_32
-#	define TCLOCK_SUFFIX " x64"
+#	define TCLOCK_SUFFIX ""
 #else
 #	define ARCH_SUFFIX ARCH_SUFFIX_64
-#	define TCLOCK_SUFFIX ""
+#	define TCLOCK_SUFFIX " x64"
 #endif // __x86_64__
 
 #define ABT_TITLE "T-Clock Redux" TCLOCK_SUFFIX " - " VER_SHORT_DOTS " build " STR(VER_REVISION)