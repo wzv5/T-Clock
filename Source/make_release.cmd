b0f035e451d3518acc5af51c4aa38c0ac4d9fb05
White-Tiger <White-Tiger@users.noreply.github.com>
2014-11-03 14:55:19 +0100

    * changed Code::Blocks project files, improved execution of pre-build steps to only run once (and not per target) * moved "AutoVersion" target to own project file + added static build project and updated make_release script * updated workspace

--- Source/make_release.cmd
+++ Source/make_release.cmd
@@ -7,5 +7,16 @@ del /S *.manifest 2>nul
 set xclude=-x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib
 7z a %xclude% T-Clock.zip .
 7z a %xclude% T-Clock.7z .
+copy *.txt ..\Static
+copy *.rtf ..\Static
+copy *.ttf ..\Static
+popd
+pushd ..\Static
+del *.zip 2>nul
+del *.7z 2>nul
+del /S *.manifest 2>nul
+set xclude=-x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib
+7z a %xclude% T-Clock_static.zip .
+rem 7z a %xclude% T-Clock_static.7z .
 popd
 pause
