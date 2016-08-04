wzv5/T-Clock

commit 9afbfdb58daffc24d94627c8ee83063e11f2a8be

[clock] fix clock hover on Win10 (1st Anniversary update)
+ clock can now change text color based on hover state (improves high contrast themes)
* also simplified our DrawXPClockHover() to handle different OS cases (instead of us before calling it)
* disabled check for high contrast themes... I don't really remember why I had to add it...

partially fix for #39, #41

src/DLL/Tclock.c
@@ -675,6 +675,8 @@ static LRESULT CALLBACK Window_Clock_Hooked(HWND hwnd, UINT message, WPARAM wPar
 			tme.hwndTrack = hwnd;
 			tme.dwHoverTime = HOVER_DEFAULT;
 			m_TipState = TrackMouseEvent(&tme);
+			if(IsXPThemeActive())
+				m_col.ref = GetXPClockColor(gs_hwndClock, CLS_HOT);
 			FillClockBGHover();
 			InvalidateRect(gs_hwndClock, NULL, 0);
 		}
@@ -705,6 +707,8 @@ static LRESULT CALLBACK Window_Clock_Hooked(HWND hwnd, UINT message, WPARAM wPar
 					PostMessage(gs_hwndClock, WM_USER+103,0,0);//hide system tooltip
 			}
 			m_TipState = 0;
+			if(IsXPThemeActive())
+				m_col.ref = GetXPClockColor(gs_hwndClock, CLS_NORMAL);
 			FillClockBG();
 			InvalidateRect(gs_hwndClock,NULL,0);
 		}
@@ -1056,7 +1060,7 @@ void FillClockBG()
 void FillClockBGHover()
 {
 	if(!m_colorBG_start) return;
-	if(api.OS>=TOS_WIN7 && IsXPThemeActive()) {
+	if(IsXPThemeActive()) {
 		DrawXPClockHover(gs_hwndClock,m_hdcClockBG,&m_rcClock);
 	}
 }

src/DLL/clock_color.c
@@ -71,7 +71,7 @@ unsigned Clock_GetColor(unsigned color, int use_raw)
 		if(use_raw)
 			return color;
 		if(IsXPThemeActive() && gs_hwndClock)
-			return GetXPClockColor(gs_hwndClock);
+			return GetXPClockColor(gs_hwndClock, CLS_NORMAL);
 		else
 			return GetSysColor(COLOR_WINDOWTEXT);
 	case TCOLOR_TRANSPARENT:
@@ -94,7 +94,7 @@ unsigned Clock_GetColor(unsigned color, int use_raw)
 		if(use_raw==2)
 			return color;
 		if(IsXPThemeActive() && gs_hwndClock)
-			return GetXPClockColorBG(gs_hwndClock);
+			return GetXPClockColorBG(gs_hwndClock, CLS_NORMAL);
 		else
 			return GetSysColor(COLOR_3DFACE);
 	}

src/common/newapi.c
@@ -6,6 +6,8 @@
 #include <uxtheme.h>
 #include "../common/utl.h"
 
+#include "newapi.h"
+
 int IsWow64(){
 	int ret=0;
 	#ifndef _WIN64
@@ -41,7 +43,8 @@ pSetLayeredWindowAttributes_t pSetLayeredWindowAttributes=NULL;
 #define THEME_FUNC_CHECK_THEME(name,hwnd,ret) \
 	THEME_FUNC_CHECK(name,ret)\
 	if(!hClockTheme){\
-		hClockTheme=pOpenThemeData(hwnd,CLOCK);/*TrayClockWClass*/\
+		hClockTheme = pOpenThemeData(hwnd,(m_theme_version>=TV_10_beta ? VSCLASS_TASKBAND2 : VSCLASS_CLOCK));\
+		m_theme_clock_part = (m_theme_version>=TV_10 ? 5 : CLP_TIME);\
 		if(!hClockTheme) return ret;\
 	}
 #define THEME_FUNC_DEFINE(name,ret,params) \
@@ -55,11 +58,23 @@ THEME_FUNC_DEFINE(OpenThemeData,HTHEME,(HWND hwnd, LPCWSTR pszClassList));
 THEME_FUNC_DEFINE(IsThemeActive,BOOL,());
 THEME_FUNC_DEFINE(SetWindowTheme,HRESULT,(HWND hwnd, LPCWSTR pszSubAppName, LPCWSTR pszSubIdList));
 THEME_FUNC_DEFINE(GetThemeColor,HRESULT,(HTHEME hTheme, int iPartId, int iStateId, int iPropId, COLORREF* pColor));
+//THEME_FUNC_DEFINE(IsThemePartDefined,BOOL,(HTHEME hTheme, int iPartId, int iStateId));
+//THEME_FUNC_DEFINE(IsThemeBackgroundPartiallyTransparent,BOOL,(HTHEME hTheme, int iPartId, int iStateId));
 THEME_FUNC_DEFINE(DrawThemeBackground,HRESULT,(HTHEME hTheme, HDC hdc, int iPartId, int iStateId, LPCRECT pRect, LPCRECT pClipRect));
 THEME_FUNC_DEFINE(DrawThemeParentBackground,HRESULT,(HWND hwnd, HDC hdc, RECT* prc));
 
-static BOOL bInitLayeredWindow = FALSE;
-static BOOL bInitDrawTheme = FALSE;
+static char bInitLayeredWindow = 0;
+static uint16_t m_theme_version = 0;
+static char m_theme_clock_part = CLP_TIME;
+#define TV_2000 0x0500
+#define TV_XP 0x0501
+#define TV_XP_64 0x0502
+#define TV_Vista 0x0600
+#define TV_7 0x0601
+#define TV_8 0x0602
+#define TV_8_1 0x0603
+#define TV_10_beta 0x0604
+#define TV_10 0x0a00
 
 static void RefreshRebar(HWND hwndBar);
 
@@ -74,7 +89,7 @@ void InitLayeredWindow(void)
 			FreeLibrary(hmodUSER32); hmodUSER32 = NULL;
 		}
 	}
-	bInitLayeredWindow = TRUE;
+	bInitLayeredWindow = 1;
 }
 
 void EndNewAPI(HWND hwndClock)
@@ -98,10 +113,12 @@ void EndNewAPI(HWND hwndClock)
 	/// DrawTheme
 	THEME_FUNC_RELEASE(DrawThemeParentBackground);
 	THEME_FUNC_RELEASE(DrawThemeBackground);
+//	THEME_FUNC_RELEASE(IsThemeBackgroundPartiallyTransparent);
+//	THEME_FUNC_RELEASE(IsThemePartDefined);
 	THEME_FUNC_RELEASE(GetThemeColor);
-	THEME_FUNC_RELEASE(OpenThemeData);
-	THEME_FUNC_RELEASE(SetWindowTheme);
 	THEME_FUNC_RELEASE(IsThemeActive);
+	THEME_FUNC_RELEASE(SetWindowTheme);
+	THEME_FUNC_RELEASE(OpenThemeData);
 	if(hClockTheme){
 		pCloseThemeData(hClockTheme); hClockTheme=NULL;
 	}
@@ -194,37 +211,24 @@ void RefreshRebar(HWND hwndBar)
 
 void InitDrawTheme()
 {
-	if(bInitDrawTheme) return;
-	bInitDrawTheme=1;
-	hmodUxTheme=LoadLibraryA("uxtheme");
+	if(m_theme_version)
+		return;
+	m_theme_version = GetVersion() & 0xffff;
+	m_theme_version = ((m_theme_version & 0xff) << 8 | m_theme_version >> 8);
+	hmodUxTheme = LoadLibraryA("uxtheme");
 	if(hmodUxTheme){
 		THEME_FUNC_RETRIEVE(CloseThemeData);
 		THEME_FUNC_RETRIEVE(OpenThemeData);
 		THEME_FUNC_RETRIEVE(IsThemeActive);
 		THEME_FUNC_RETRIEVE(SetWindowTheme);
 		THEME_FUNC_RETRIEVE(GetThemeColor);
+//		THEME_FUNC_RETRIEVE(IsThemePartDefined);
+//		THEME_FUNC_RETRIEVE(IsThemeBackgroundPartiallyTransparent);
 		THEME_FUNC_RETRIEVE(DrawThemeBackground);
 		THEME_FUNC_RETRIEVE(DrawThemeParentBackground);
 	}
 }
 
-/// Win7+ clock defines
-#define TASKBAND L"TaskBand2"
-#define CLOCK L"Clock"
-#ifndef CLP_TIME
-#	define CLP_TIME 1
-#	define CLS_NORMAL 1
-#	define CLS_HOT 2
-#	define CLS_PRESSED 3
-//#	define TMT_COLOR 204
-#	define TMT_BACKGROUND 1602
-#	define TMT_TEXTCOLOR 3803
-
-#	define TMT_TRANSPARENTCOLOR 3809
-#	define TMT_WINDOW 1606
-#	define TMT_WINDOWFRAME 1607
-#	define TMT_FILLCOLOR 3802
-#endif
 void ReloadXPClockTheme()
 {
 	THEME_FUNC_CHECK(OpenThemeData,)
@@ -233,37 +237,43 @@ void ReloadXPClockTheme()
 		hClockTheme=NULL;
 	}
 }
-COLORREF GetXPClockColor(HWND hwndClock)
+COLORREF GetXPClockColor(HWND hwndClock, int state)
 {
 	COLORREF ret;
-	THEME_FUNC_CHECK_THEME(GetThemeColor,hwndClock,0x00FFFFFF)
-	pGetThemeColor(hClockTheme,CLP_TIME,CLS_NORMAL,TMT_TEXTCOLOR,&ret);
+	THEME_FUNC_CHECK_THEME(GetThemeColor, hwndClock, 0x00FFFFFF)
+	pGetThemeColor(hClockTheme, m_theme_clock_part, state, TMT_TEXTCOLOR, &ret);
 	return ret;
 }
-COLORREF GetXPClockColorBG(HWND hwndClock)
+COLORREF GetXPClockColorBG(HWND hwndClock, int state)
 {
 	COLORREF ret;
-	THEME_FUNC_CHECK_THEME(GetThemeColor,hwndClock,0x00000000)
-	pGetThemeColor(hClockTheme,CLP_TIME,CLS_NORMAL,TMT_BACKGROUND,&ret);
+	THEME_FUNC_CHECK_THEME(GetThemeColor, hwndClock, 0x00000000)
+	pGetThemeColor(hClockTheme, m_theme_clock_part, state, TMT_BACKGROUND, &ret);
 	return ret;
 }
 void DrawXPClockBackground(HWND hwnd, HDC hdc, RECT* prc)
 {
 	THEME_FUNC_CHECK(DrawThemeParentBackground,)
+	THEME_FUNC_CHECK_THEME(DrawThemeBackground,hwnd,)
 	pDrawThemeParentBackground(hwnd, hdc, prc);
+	if(m_theme_version >= TV_7)
+		pDrawThemeBackground(hClockTheme, hdc, m_theme_clock_part, CLS_NORMAL, prc, NULL);
 }
 void DrawXPClockHover(HWND hwnd, HDC hdc, RECT* prc)
 {
+	THEME_FUNC_CHECK(DrawThemeParentBackground,)
 	THEME_FUNC_CHECK_THEME(DrawThemeBackground,hwnd,)
-	pDrawThemeBackground(hClockTheme,hdc,CLP_TIME,CLS_HOT,prc,NULL);
+	pDrawThemeParentBackground(hwnd, hdc, prc);
+	if(m_theme_version >= TV_7)
+		pDrawThemeBackground(hClockTheme, hdc, m_theme_clock_part, CLS_HOT, prc, NULL);
 }
 
 BOOL IsXPThemeActive(){
-	HIGHCONTRAST highcontContrast={sizeof(HIGHCONTRAST)};
+//	HIGHCONTRAST highcontContrast = {sizeof(HIGHCONTRAST)};
 	THEME_FUNC_CHECK(IsThemeActive,0)
-	SystemParametersInfo(SPI_GETHIGHCONTRAST,sizeof(HIGHCONTRAST),&highcontContrast,0);
-	if(highcontContrast.dwFlags&HCF_HIGHCONTRASTON)
-		return 0;
+//	SystemParametersInfo(SPI_GETHIGHCONTRAST, sizeof(HIGHCONTRAST), &highcontContrast, 0);
+//	if(highcontContrast.dwFlags & HCF_HIGHCONTRASTON)
+//		return 0;
 	return pIsThemeActive();
 }
 HRESULT SetXPWindowTheme(HWND hwnd, LPCWSTR pszSubAppName, LPCWSTR pszSubIdList){

src/common/newapi.h
@@ -23,20 +23,43 @@ void SetLayeredTaskbar(HWND hwndClock, int alpha, int clear_taskbar, int refresh
 //void TC2DrawBlt(HDC dhdc, int dx, int dy, int dw, int dh, HDC shdc, int sx, int sy, int sw, int sh, BOOL useTrans);
 
 // DrawTheme
+#define VSCLASS_CLOCK L"Clock"
+#define VSCLASS_TASKBAND2 L"TaskBand2" /**< Win10+ */
+
+#ifndef CLP_TIME
+#	define CLP_TIME 1
+#	define CLS_NORMAL 1
+#	define CLS_HOT 2
+#	define CLS_PRESSED 3
+//#	define TMT_COLOR 204
+#	define TMT_BACKGROUND 1602
+#	define TMT_WINDOWTEXT 1609
+#	define TMT_CAPTIONTEXT 1610
+#	define TMT_BTNTEXT 1619
+#	define TMT_INFOTEXT 1624
+#	define TMT_TEXTCOLOR 3803
+
+#	define TMT_TRANSPARENTCOLOR 3809
+#	define TMT_WINDOW 1606
+#	define TMT_WINDOWFRAME 1607
+#	define TMT_FILLCOLOR 3802
+#endif
 
 /**
  * \brief get current theme color for our clock text
  * \param hwndClock handle to our clock
+ * \param state one of \c CLS_NORMAL, \c CLS_HOT or \c CLS_PRESSED
  * \return COLORREF with our text color
- * \sa GetXPClockColorBG(), ReloadXPClockTheme() */
-COLORREF GetXPClockColor(HWND hwndClock);
+ * \sa GetXPClockColorBG(), ReloadXPClockTheme(), CLS_NORMAL, CLS_HOT, CLS_PRESSED */
+COLORREF GetXPClockColor(HWND hwndClock, int state);
 /**
  * \brief get current theme color for our clock background
  * \param hwndClock handle to our clock
+ * \param state one of \c CLS_NORMAL, \c CLS_HOT or \c CLS_PRESSED
  * \return COLORREF with our background color
  * \remarks might not work properly on XP
- * \sa GetXPClockColor(), ReloadXPClockTheme() */
-COLORREF GetXPClockColorBG(HWND hwndClock);
+ * \sa GetXPClockColor(), ReloadXPClockTheme(), CLS_NORMAL, CLS_HOT, CLS_PRESSED */
+COLORREF GetXPClockColorBG(HWND hwndClock, int state);
 /**
  * \brief reloads current theme information used by \c GetXPClockColor() and \c GetXPClockColorBG()
  * \sa GetXPClockColor(), GetXPClockColorBG() */

commit 513be2dcf81758cee35752d6fc546e632cd38a73

[clock] fixed custom clock colors on hover, broken in 9afbfdb, fixes #43
small improvements to api.GetColor()
reverted back to previous hover background drawing...

src/DLL/Tclock.c
@@ -62,28 +62,34 @@ static int m_vertfeed,m_vertpos;
 static int m_horizfeed,m_horizpos;
 /// colors
 COLORREF m_basecolorBG, m_basecolorFont;
-typedef struct tagBGRQUAD{
+typedef struct BGRQUAD {
 	BYTE rgbRed;
 	BYTE rgbGreen;
 	BYTE rgbBlue;
 	BYTE rgbReserved;
 } BGRQUAD;
-union{
+typedef union COLOR {
 	BGRQUAD quad;
 	COLORREF ref;
-} m_col;
-union{
-	BGRQUAD quad;
-	COLORREF ref;
-} m_colBG;
-#define ColorUpdate(col,colBG) do{\
-	COLORREF oldbg;\
-	m_col.ref = api.GetColor(col, 0);\
-	oldbg = m_colBG.ref;\
-	m_colBG.ref = api.GetColor(colBG, 1);\
-	if(m_colBG.ref != oldbg)\
-		FillClockBG();\
-	} __pragma(warning(suppress:4127)) while(0)
+} COLOR;
+COLOR m_col;
+COLOR m_col_normal;
+COLOR m_col_hover;
+COLOR m_colBG;
+
+void ColorUpdate(unsigned text_color, unsigned back_color) {
+	COLORREF old;
+	m_col_normal.ref = m_col.ref = api.GetColor(text_color, 0);
+	if(text_color == TCOLOR(TCOLOR_DEFAULT) || text_color == TCOLOR(TCOLOR_TRANSPARENT))
+		m_col_hover.ref = api.GetColor(TCOLOR(TCOLOR_DEFAULT), TCOLORFLAG_HOVER);
+	else
+		m_col_hover = m_col_normal;
+	old = m_colBG.ref;
+	m_colBG.ref = api.GetColor(back_color, TCOLORFLAG_RAW1);
+	if(m_colBG.ref != old)
+		FillClockBG();
+}
+
 /*** misc variables ***/
 static long m_clock_base_width; ///< original clock's width used by taskbar calculation
 static long m_clock_base_height; ///< original clock's height used by taskbar calculation
@@ -767,8 +773,7 @@ static LRESULT CALLBACK Window_Clock_Hooked(HWND hwnd, UINT message, WPARAM wPar
 			tme.hwndTrack = hwnd;
 			tme.dwHoverTime = HOVER_DEFAULT;
 			m_TipState = TrackMouseEvent(&tme);
-			if(IsXPThemeActive())
-				m_col.ref = GetXPClockColor(gs_hwndClock, CLS_HOT);
+			m_col = m_col_hover;
 			FillClockBGHover();
 			InvalidateRect(gs_hwndClock, NULL, 0);
 		}
@@ -799,8 +804,7 @@ static LRESULT CALLBACK Window_Clock_Hooked(HWND hwnd, UINT message, WPARAM wPar
 					PostMessage(gs_hwndClock, WM_USER+103,0,0);//hide system tooltip
 			}
 			m_TipState = 0;
-			if(IsXPThemeActive())
-				m_col.ref = GetXPClockColor(gs_hwndClock, CLS_NORMAL);
+			m_col = m_col_normal;
 			FillClockBG();
 			InvalidateRect(gs_hwndClock,NULL,0);
 		}

src/DLL/clock_color.c
@@ -56,26 +56,31 @@ void Clock_On_DWMCOLORIZATIONCOLORCHANGED(unsigned argb, BOOL blend) { /// there
 	col.quad.rgbReserved = 0x00;
 	m_themecolor = col.ref;
 }
-unsigned Clock_GetColor(unsigned color, int use_raw)
+unsigned Clock_GetColor(unsigned color, int flags)
 {
 	// useraw = 1 == want some raw values, mainly "default"
 	// useraw = 2 == always want raw values (that is, everytime a TCOLOR_* isn't save to be right)
 	unsigned sub;
+	int state = CLS_NORMAL;
 	if((color&TCOLOR_MASK)!=TCOLOR_MAGIC)
 		return color;
-	sub=color^TCOLOR_MAGIC;
-	if(sub<TCOLOR_BEGIN_)
+	if(flags & TCOLORFLAG_HOVER)
+		state = CLS_HOT;
+//	if(flags & TCOLORFLAG_PRESSED)
+//		state = CLS_PRESSED;
+	sub = color^TCOLOR_MAGIC;
+	if(sub < TCOLOR_BEGIN_)
 		return GetSysColor(sub);
 	switch(sub){
 	case TCOLOR_DEFAULT:
-		if(use_raw)
+		if(flags & (TCOLORFLAG_RAW1|TCOLORFLAG_RAW2))
 			return color;
 		if(IsXPThemeActive() && gs_hwndClock)
-			return GetXPClockColor(gs_hwndClock, CLS_NORMAL);
+			return GetXPClockColor(gs_hwndClock, state);
 		else
 			return GetSysColor(COLOR_WINDOWTEXT);
 	case TCOLOR_TRANSPARENT:
-		if(use_raw)
+		if(flags & (TCOLORFLAG_RAW1|TCOLORFLAG_RAW2))
 			return color;
 		return 0xFF000000;
 	case TCOLOR_THEME:
@@ -91,10 +96,10 @@ unsigned Clock_GetColor(unsigned color, int use_raw)
 			Clock_On_DWMCOLORIZATIONCOLORCHANGED(0, 1);
 		return m_themecolor_alpha;
 	case TCOLOR_THEME_BG:
-		if(use_raw==2)
+		if(flags & TCOLORFLAG_RAW2)
 			return color;
 		if(IsXPThemeActive() && gs_hwndClock)
-			return GetXPClockColorBG(gs_hwndClock, CLS_NORMAL);
+			return GetXPClockColorBG(gs_hwndClock, state);
 		else
 			return GetSysColor(COLOR_3DFACE);
 	}

src/common/clock.h
@@ -25,6 +25,12 @@ enum TCOLORS {
 	TCOLOR_THEME_BG, /**< meta, current theme's clock background color <b>failed attempt and useless</b> */
 	TCOLOR_END_,
 };
+enum TCOLORFLAGS {
+	TCOLORFLAG_NORMAL = 0x00, /**< returns colors directly for drawing */
+	TCOLORFLAG_RAW1 = 0x01, /**< return \c TCOLOR_DEFAULT and \c TCOLOR_TRANSPARENT directly \sa TCOLORS */
+	TCOLORFLAG_RAW2 = 0x02, /**< return even more directly (except theme colors) \sa TCOLORS */
+	TCOLORFLAG_HOVER = 0x04, /**< use hover colors for \c TCOLOR_DEFAULT \sa TCOLOR_DEFAULT */
+};
 
 enum TOS {
 	TOS_OLDER	=0x0000, /**< unknown (older) OS or error \sa TClockAPI::OS */
@@ -102,13 +108,13 @@ struct TClockAPI {
 	int (*GetFileAndOption)(const wchar_t* command, wchar_t* app, wchar_t* params);
 /**
  * \brief parses given color ( \c COLORREF ) for use by T-Clock or Windows
- * \param color color to parse (can be either one of \c TCOLORS, a Windows system color or a user defined color)
- * \param use_raw \c 1 if raw color should be returned, eg. "unparsed" for some \c TCOLOR_* values that wouldn't be good to preview
+ * \param color the color to parse (can be either one of \c TCOLORS, a Windows system color or a user defined color)
+ * \param flags any of \c TCOLORFLAGS
  * \return parsed color value
- * \remark \a use_raw currently only applies to \c TCOLOR_DEFAULT and \c TCOLOR_TRANSPARENT
+ * \remark \a flags are mostly for \c TCOLOR_DEFAULT
  * \remark \b we use the highest byte as a transparency value, ranging from 0-255, while 255 means fully transparent
- * \sa TCOLORS, On_DWMCOLORIZATIONCOLORCHANGED(), COLORREF, RGB(), GetSysColor() */
-	unsigned (*GetColor)(unsigned color,int use_raw);
+ * \sa TCOLORS, TCOLORFLAGS, On_DWMCOLORIZATIONCOLORCHANGED(), COLORREF, RGB(), GetSysColor() */
+	unsigned (*GetColor)(unsigned color, int flags);
 /**
  * \brief callback for use by \c WM_DWMCOLORIZATIONCOLORCHANGED messages to read current theme's color
  * \param argb wParam of \c WM_DWMCOLORIZATIONCOLORCHANGED

src/common/control_extensions.c
@@ -154,7 +154,7 @@ static const syscolor_t m_syscolor[]={
 	{TCOLOR_THEME,L"Theme color"},
 	{TCOLOR_THEME_DARK,L"Theme color (dark)"},
 	{TCOLOR_THEME_ALPHA,L"Theme color w/ alpha"},
-//	{TCOLOR_THEME_BG,"Theme bg color"},
+//	{TCOLOR_THEME_BG,L"Theme bg color"},
 	
 	{COLOR_3DFACE,L"3DFACE"},
 	{COLOR_3DSHADOW,L"3DSHADOW"},
@@ -258,7 +258,7 @@ LRESULT CALLBACK ColorBoxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		char color_str[8];
 		HGLOBAL hg;
 		char* pbuf;
-		unsigned color = api.GetColor(ColorBox_GetColorRaw(hwnd),2);
+		unsigned color = api.GetColor(ColorBox_GetColorRaw(hwnd), TCOLORFLAG_RAW2);
 		color = ((color&0xff)<<16) | (color&0xff00) | ((color&0xff0000)>>16);
 		__pragma(warning(suppress:4996)) sprintf(color_str, "#%06x", color);
 		if(!OpenClipboard(hwnd))
@@ -309,7 +309,7 @@ LRESULT CALLBACK ColorBoxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				}
 				if(!size){ // valid hex value
 					for(size=TCOLOR_BEGIN_; size<TCOLOR_END_; ++size){
-						if(color == (api.GetColor(TCOLOR(size),2) & 0xffffff))
+						if(color == (api.GetColor(TCOLOR(size),TCOLORFLAG_RAW2) & 0xffffff))
 							break;
 					}
 					if(size != TCOLOR_END_)
@@ -362,7 +362,7 @@ LRESULT ColorBox_OnDrawItem(WPARAM wParam, LPARAM lParam) {
 	color_rc.bottom = color_rc.top + draw_rc.bottom;
 	
 	if(IsWindowEnabled(pdis->hwndItem)) {
-//		col = api.GetColor((COLORREF)pdis->itemData,2);
+//		col = api.GetColor((COLORREF)pdis->itemData, TCOLORFLAG_RAW);
 //		if((col&TCOLOR_MASK)==TCOLOR_MAGIC)
 //			col &= 0x00ffffff;
 		col = api.GetColor((COLORREF)pdis->itemData,0);
@@ -443,7 +443,7 @@ int ColorBox_ChooseColor(HWND button)
 	HWND color_cb = GetWindow(button, GW_HWNDPREV);
 	size_t idx;
 	
-	color = api.GetColor((COLORREF)ComboBox_GetItemData(color_cb, ComboBox_GetCurSel(color_cb)),2);
+	color = api.GetColor((COLORREF)ComboBox_GetItemData(color_cb, ComboBox_GetCurSel(color_cb)), TCOLORFLAG_RAW2);
 	
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms646908%28v=vs.85%29.aspx
 	//   https://msdn.microsoft.com/en-us/library/ms646869%28v=vs.85%29.aspx

src/common/newapi.c
@@ -261,9 +261,9 @@ void DrawXPClockBackground(HWND hwnd, HDC hdc, RECT* prc)
 }
 void DrawXPClockHover(HWND hwnd, HDC hdc, RECT* prc)
 {
-	THEME_FUNC_CHECK(DrawThemeParentBackground,)
+	//THEME_FUNC_CHECK(DrawThemeParentBackground,)
 	THEME_FUNC_CHECK_THEME(DrawThemeBackground,hwnd,)
-	pDrawThemeParentBackground(hwnd, hdc, prc);
+	//pDrawThemeParentBackground(hwnd, hdc, prc);
 	if(m_theme_version >= TV_7)
 		pDrawThemeBackground(hClockTheme, hdc, m_theme_clock_part, CLS_HOT, prc, NULL);
 }