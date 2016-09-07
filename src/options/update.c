7b7c0f04f28f6f391d19c226153c7043dbc5cb02
White-Tiger <White-Tiger@users.noreply.github.com>
2016-09-07 13:45:43 +0200

    [update] UTF-8 support for update notify
    ! also fixed a buffer size calculation.. I wanted 512 additional bytes for the message header and stuff, but I've got (512*2048 - 512 - 2048) more than I wanted... and wouldn't even be able to use them no matter what. Seems like a simple 512 + 2048 is too hard for me after all...

--- src/options/update.c
+++ src/options/update.c
@@ -77,18 +77,32 @@ void UpdateCheck_Free(UpdateData* data) {
 }
 
 int UpdateCheck_WriteDescription(wchar_t* out, int size, const char* text) {
-	int written = 0;
+	static const wchar_t kPadding[4] = {' ', ' ', ' ', ' '};
+	int remaining = --size;
+	int num;
 	wchar_t* pos = out;
 	char* nl;
-	do{
+	while(remaining > _countof(kPadding)){
+		memcpy(pos, kPadding, sizeof(kPadding));
+		remaining -= _countof(kPadding);
+		pos += _countof(kPadding);
+		
 		nl = strchr(text, '\n');
 		if(!nl++)
 			nl = strchr(text, '\0');
-		pos += swprintf(pos, size-written, FMT("   %.*hs"), nl-text, text);
-		text = nl;;
-		written = pos-out;
-	}while(*nl);
-	return written;
+		num = MultiByteToWideChar(CP_UTF8, 0, text, (nl - text), pos, remaining);
+		remaining -= num;
+		pos += num;
+		if(!num || !*nl)
+			break;
+		text = nl;
+	}
+	*pos = '\0';
+//	for(; *out; ++out) {
+//		if(*out == ' ')
+//			*out = L' ';
+//	}
+	return (size - remaining);
 }
 
 // https://msdn.microsoft.com/en-us/library/windows/desktop/aa383138%28v=vs.85%29.aspx
@@ -333,8 +347,8 @@ static INT_PTR CALLBACK Window_UpdateCheckDlg(HWND hDlg, UINT uMsg, WPARAM wPara
 				if(data->type != UPDATE_SILENT) {
 					wchar_t* abovebehind;
 					wchar_t* msg,* msg_pos,* msg_end;
-					msg = msg_pos = (wchar_t*)malloc((512*UPDATE_BUFFER) * sizeof(msg[0]));
-					msg_end = msg + UPDATE_BUFFER;
+					msg = msg_pos = (wchar_t*)malloc((512 + UPDATE_BUFFER) * sizeof(msg[0]));
+					msg_end = msg + (512 + UPDATE_BUFFER);
 					if(!msg) {
 						SendMessage(hDlg, WM_DOWNLOAD_RESULT, 2, 0);
 						return 1;
