wzv5/T-Clock

commit ae8235524bd0910f8854df3a7e385ecb2f98dc6d

minor source code optimizations
* added "static" keyword to some constant function variables
* silenced a MSVC warning (implicit type cast; format.c)

src/Clock/pageformat.c
@@ -124,7 +124,7 @@ void ChecksLocaleInit(char checks[GROUP_FORMAT_NUM], int ilang/*=0*/)
 {
 	wchar_t ampm[10];
 	int ival;
-	const int aLangDayOfWeekIsLast[] = {LANG_JAPANESE,LANG_KOREAN};
+	static const int aLangDayOfWeekIsLast[] = {LANG_JAPANESE, LANG_KOREAN};
 	char bTimeMarker; // use AM/PM (+12h format)
 	
 	if(ilang)
@@ -226,8 +226,8 @@ BOOL CALLBACK EnumLocalesProc(wchar_t* lpLocaleString)
 --------------------------------------------------*/
 void OnInit(HWND hDlg)
 {
-	const wchar_t* AM[] = {L"AM", L"am", L"A", L"a", L" ",};
-	const wchar_t* PM[] = {L"PM", L"pm", L"P", L"p", L" ",};
+	static const wchar_t* AM[] = {L"AM", L"am", L"A", L"a", L" ",};
+	static const wchar_t* PM[] = {L"PM", L"pm", L"P", L"p", L" ",};
 	const int AMPMs = _countof(AM);
 	HWND doc_lnk = GetDlgItem(hDlg, IDC_FORMAT_LNK);
 	HWND format_cb = GetDlgItem(hDlg, IDC_FORMAT);
@@ -472,7 +472,7 @@ void InitFormat()
 --------------------------------------------------*/
 void CreateFormat(wchar_t* dst, char* checks)
 {
-	const wchar_t* spacer = L" ";
+	static const wchar_t* spacer = L" ";
 	char use_time = 0; ///< bitmask; 1 = date, 2 = time
 	int control;
 	int creation_bit; ///< date/time bits; &1 = date, !&1 = time

src/Clock/BounceWind.c
@@ -205,7 +205,7 @@ void OnOK(HWND hDlg)   //----------------------------------------------------+++
 //----------------------------+++--> Parse the Dialoggie Settings Out of the String:
 void ParseSettings(wchar_t* data)   //------------------------------------------+++-->
 {
-	const wchar_t seps[] = L",";
+	static const wchar_t seps[] = L",";
 	wchar_t* szToken, *nxToken;
 	int i=0;
 	

src/Clock/pagecolor.c
@@ -340,7 +340,7 @@ int CALLBACK EnumFontFamExProc(const LOGFONT* lpelfe, const TEXTMETRIC* lpntme, 
 int CALLBACK EnumSizeProcEx(const LOGFONT* lpelfe, const TEXTMETRIC* lpntme, DWORD FontType, LPARAM lParam)
 {
 	HWND hwndSize = (HWND)lParam;
-	const unsigned char nFontSizes[] = {4,5,6,7,8,9,10,11,12,13,14,15,16,18,20,22,24,26,28,32,36,48,72};
+	static const unsigned char nFontSizes[] = {4,5,6,7,8,9,10,11,12,13,14,15,16,18,20,22,24,26,28,32,36,48,72};
 	wchar_t str[8];
 	int i, size, count;
 	(void)lpelfe;

src/Clock/propsheet.c
@@ -35,7 +35,7 @@ char g_bApplyClock = 0;
 //----------------------------//-----------------------+++--> Show the (Tab Dialog) Property Sheet:
 void MyPropertySheet(int page)   //---------------------------------------------------------+++-->
 {
-	const DLGPROC PageProc[]={
+	static const DLGPROC PageProc[]={
 		Page_About, Page_Alarm,
 		Page_Color, Page_Format, Page_Mouse,
 		Page_Quicky,

src/Clock/timer.c
@@ -802,7 +802,7 @@ static int OnWatchTimer(HWND hwnd) {
 	return count;
 }
 static void RemoveFromWatch(HWND hWnd, HWND list, int index) {
-	const wchar_t message[] = L"Yes will cancel the timer & remove it from the Watch List\n"
+	static const wchar_t message[] = L"Yes will cancel the timer & remove it from the Watch List\n"
 							L"No will remove timer from Watch List only (timer continues)\n"
 							L"Cancel will assume you hit delete accidentally (and do nothing)";
 	wchar_t caption[TNY_BUFF + kTimerNameLen];

src/DLL/format.c
@@ -349,7 +349,7 @@ unsigned MakeFormat(wchar_t buf[FORMAT_MAX_SIZE], const wchar_t* fmt, SYSTEMTIME
 				++fmt;
 				if(!weekday && *fmt == 'i')
 					weekday = 7;
-				*out++ = '0' + weekday;
+				*out++ = '0' + (char)weekday;
 			} else {
 				*out++ = 'w';
 			}

src/common/utl.c
@@ -384,7 +384,7 @@ HWND CreateDialogParamOnce(HWND* hwnd, HINSTANCE hInstance, const wchar_t* lpTem
 
 HBITMAP CreateBitmapWithAlpha(HDC hdc, int width, int height) {
 	BITMAPINFO bmi = {0};
-	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bmi.bmiHeader.biSize = sizeof(bmi.bmiHeader);
 	bmi.bmiHeader.biPlanes = 1;
 	bmi.bmiHeader.biCompression = BI_RGB;
 	bmi.bmiHeader.biWidth = width;

src/~ide/gcc-clock.cbp
@@ -72,6 +72,7 @@
 					<Add option="-m64" />
 				</Linker>
 				<ExtraCommands>
+					<Add before='cmd /C &quot;del ..\..\Debug\Crash.log&quot;' />
 					<Add after="objcopy --only-keep-debug ../../Debug/Clock64.exe ../../Debug/Clock64.dbg" />
 					<Add after="objcopy --strip-debug --strip-unneeded --add-gnu-debuglink=../../Debug/Clock64.dbg ../../Debug/Clock64.exe" />
 				</ExtraCommands>

commit e3f27251961a9848dfe95069ae9ee2d6f879ec3a

App: fixed messed up commit ae82355 which broke "new line" format option since v2.4.3#471-beta
! the "new line" toggle worked exactly once... so basically not at all.
Why did nobody notice?

src/Clock/pageformat.c
@@ -474,7 +474,7 @@ void InitFormat()
 --------------------------------------------------*/
 void CreateFormat(wchar_t* dst, char* checks)
 {
-	static const wchar_t* spacer = L" ";
+	const wchar_t* spacer = L" ";
 	char use_time = 0; ///< bitmask; 1 = date, 2 = time
 	int control;
 	int creation_bit; ///< date/time bits; &1 = date, !&1 = time