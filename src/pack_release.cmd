1dd8b2d537440521371640b112027305cd38d9b7
White-Tiger <White-Tiger@users.noreply.github.com>
2018-05-20 19:08:44 +0200

    build: renamed `pack_ci_release.bat` to `sign-n-zip.bat`
    - removed quite obsolete pack_release.cmd

--- src/pack_release.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Program Files\7-Zip
-set "xclude=-xr!*.ini -xr!*.log -xr!*.rpt -x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib -xr!*.def -xr!*.a -xr!*.manifest -xr!_*"
-pushd ..\Release
-del *.zip *.7z 2>nul
-7z a %xclude% T-Clock.zip .
-7z a %xclude% T-Clock.7z .
-popd
-pause
