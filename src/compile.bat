wzv5/T-Clock

commit 50bf612cff8d4ef3e6d2a63f32514ef2d51b83ee

build: allow build scripts to be run from virtually anywhere

src/compile.bat
@@ -13,7 +13,7 @@ fi
 ./makex "$@"
 ret=$?
 popd
-return $ret 2>/dev/null || exit $ret
+return $ret 2>/dev/null;exit $ret
 
 
 :=kkk

src/sign-n-zip.bat
@@ -1,32 +1,32 @@
 goto=kkk
 #!/bin/bash
 # start of bash
+pushd "`dirname "${BASH_SOURCE[0]}"`/~ide"
 xclude="-xr!*.ini -xr!*.log -xr!*.rpt -x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib -xr!*.def -xr!*.a -xr!*.manifest"
 tag=${TRAVIS_TAG:-${APPVEYOR_REPO_TAG_NAME}}
-function errchk(){ e=$?; if [ $e -ne 0 ];then exit $e; fi }
 # require tag
-#[ -z "$tag" ] && exit 0
+#[ -z "$tag" ] && (return 0 2>/dev/null;exit 0)
 echo "	$tag"
 if [ "$signpwd" ];then
 	# get cert and signtool for linux
 	(pushd .. && echo "wget 'osslsigncode'"
 	wget -q "$signurl" -O .cert.zip && 7z x -yp$signpwd .cert.zip>/dev/null && chmod +x osslsigncode-src/configure
-	errchk
+	e=$?;[ $e != 0 ]&&(return $e 2>/dev/null;exit $e)
 	echo "make 'osslsigncode'" && cd osslsigncode-src && ./configure -q && make V=0 && mv osslsigncode ../ && popd)
-	errchk
+	e=$?;[ $e != 0 ]&&(return $e 2>/dev/null;exit $e)
 	# sign
 	pushd ../Release && echo "sign"
 	for f in *.exe *.dll */*.exe */*.dll;do
 		[ -f "$f" ] || continue
 		echo -n "$f: "
 		../osslsigncode sign -pkcs12 ../.cert.pfx -t http://timestamp.verisign.com/scripts/timstamp.dll "$f" "$f.tmp"
-		errchk
+		e=$?;[ $e != 0 ]&&(return $e 2>/dev/null;exit $e)
 		mv "$f.tmp" "$f"
 	done
 	popd
 fi
 # compress
-pushd ../Release && echo "compress"
+cd ../Release && echo "compress"
 rm -f *.zip *.7z
 7z a $xclude T-Clock.zip .
 ret=$?
@@ -36,12 +36,13 @@ if [ "$GDRIVE_REFRESH_TOKEN" ];then
 	./gdrive update --refresh-token $GDRIVE_REFRESH_TOKEN "1m18Jb-eZya6to3NsXUlZeC2ITjXdM7IU" T-Clock.zip
 fi
 popd
-return $ret 2>/dev/null || exit $ret
+return $ret 2>/dev/null;exit $ret
 
 
 :=kkk
 @echo off
 rem # start of batch
+pushd %~dp0~ide
 if not defined signtool set signtool=signtool
 set "xclude=-xr!*.ini -xr!*.log -xr!*.rpt -x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib -xr!*.def -xr!*.a -xr!*.manifest -xr!_*"
 if defined TRAVIS_TAG set tag=%TRAVIS_TAG%
@@ -65,7 +66,7 @@ if defined signpwd (
 	popd
 )
 rem # compress
-pushd ..\Release && echo compress
+cd ..\Release && echo compress
 del *.zip *.7z 2>nul
 7z a %xclude% T-Clock_vc2010.zip .
 set ret=%errorlevel%

commit 2eb7e2aaf48af0b20bedaff5d5d629216011b382

build: fixed sign-n-zip.bat (broken by 50bf612); LOGGING environmental variable added

src/compile.bat
@@ -7,7 +7,10 @@ export TRAVIS_COMMIT_MSG="${TRAVIS_COMMIT_MSG:-$(git log --format=%B --no-merges
 AUVER_IF_NOT=${TRAVIS_TAG:-${APPVEYOR_REPO_TAG_NAME}}
 if [ -z "$AUVER_IF_NOT" ];then export AUVER_SET="STATUS=0"; fi
 if [[ $TRAVIS_COMMIT_MSG =~ \[(log(ging)?)\] ]];then
-	echo "#define LOGGING" > common/utl_logging.h
+	LOGGING=1
+fi
+if [ "$LOGGING" = 1 ];then
+	echo "#define LOGGING" > ../common/utl_logging.h
 fi
 
 ./makex "$@"
@@ -31,11 +34,13 @@ rem if not defined APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED (
 	git log --format^=%%B --no-merges -n1 >.tmp_msg
 	(findstr /C:"[logging]" .tmp_msg || findstr /C:"[log]" .tmp_msg) >nul
 	if %errorlevel% equ 0 (
-		echo #define LOGGING > common/utl_logging.h
+		set LOGGING=1
 	)
 	del .tmp_msg
 rem )
-
+if "%LOGGING%"=="1" (
+	echo #define LOGGING > ../common/utl_logging.h
+)
 if defined APPVEYOR (
 	if not defined MSBUILD_LOGGER set "MSBUILD_LOGGER=/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll""
 )

src/sign-n-zip.bat
@@ -1,7 +1,7 @@
 goto=kkk
 #!/bin/bash
 # start of bash
-pushd "`dirname "${BASH_SOURCE[0]}"`/~ide"
+pushd "`dirname "${BASH_SOURCE[0]}"`"
 xclude="-xr!*.ini -xr!*.log -xr!*.rpt -x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib -xr!*.def -xr!*.a -xr!*.manifest"
 tag=${TRAVIS_TAG:-${APPVEYOR_REPO_TAG_NAME}}
 # require tag
@@ -15,7 +15,7 @@ if [ "$signpwd" ];then
 	echo "make 'osslsigncode'" && cd osslsigncode-src && ./configure -q && make V=0 && mv osslsigncode ../ && popd)
 	e=$?;[ $e != 0 ]&&(return $e 2>/dev/null;exit $e)
 	# sign
-	pushd ../Release && echo "sign"
+	pushd ../Release; echo "sign"
 	for f in *.exe *.dll */*.exe */*.dll;do
 		[ -f "$f" ] || continue
 		echo -n "$f: "
@@ -26,7 +26,7 @@ if [ "$signpwd" ];then
 	popd
 fi
 # compress
-cd ../Release && echo "compress"
+cd ../Release; echo "compress"
 rm -f *.zip *.7z
 7z a $xclude T-Clock.zip .
 ret=$?
@@ -42,14 +42,14 @@ return $ret 2>/dev/null;exit $ret
 :=kkk
 @echo off
 rem # start of batch
-pushd %~dp0~ide
+pushd %~dp0
 if not defined signtool set signtool=signtool
 set "xclude=-xr!*.ini -xr!*.log -xr!*.rpt -x!*.zip -xr!*.pdb -xr!*.exp -xr!*.lib -xr!*.def -xr!*.a -xr!*.manifest -xr!_*"
 if defined TRAVIS_TAG set tag=%TRAVIS_TAG%
 if defined APPVEYOR_REPO_TAG_NAME set tag=%APPVEYOR_REPO_TAG_NAME%
 rem # require tag
 rem if not defined tag exit /B 0
-echo 	%tag%
+echo.	%tag%
 if defined signpwd (
 	rem # get cert
 	pushd .. && echo wget '.cert.zip'
@@ -58,7 +58,7 @@ if defined signpwd (
 	if not exist .cert.pfx exit /B 666
 	popd
 	rem # sign
-	pushd ..\Release && echo sign
+	pushd ..\Release & echo sign
 	%signtool% sign /v /f ..\.cert.pfx /t http://timestamp.verisign.com/scripts/timstamp.dll *.exe misc\*.exe misc\*.dll
 	if %errorlevel% neq 0 exit /B %errorlevel%
 	rem %signtool% verify Clock.exe
@@ -66,7 +66,7 @@ if defined signpwd (
 	popd
 )
 rem # compress
-cd ..\Release && echo compress
+cd ..\Release & echo compress
 del *.zip *.7z 2>nul
 7z a %xclude% T-Clock_vc2010.zip .
 set ret=%errorlevel%